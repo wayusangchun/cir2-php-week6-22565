de804fabdfb5792ee81eb540f58a3cd0a9fd0a8e
phisit77 <kachidoki787@gmail.com>
2022-11-24 17:13:22 +0700

    update

---
+++ week601.php
@@ -0,0 +1,9 @@
+<?php
+$i = 1;
+for($i; $i<=12; $i = $i+1){
+    print("2 x$i = ".(2*$i)."<br/>");
+    $total += $i;
+}
+print("Total = ".$total);
+
+
---
+++ week602.php
@@ -0,0 +1,7 @@
+<?php
+$season =array("Summer","Winter","Autumn");
+
+foreach($season as $arr){
+    print("Season is : $arr<br />");
+}
+?>
